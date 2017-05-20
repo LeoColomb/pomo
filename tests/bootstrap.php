0980fd0b074e3669489eea73a94782a31defc59c
ozh <ozh@ozh.org>
2017-05-20 15:21:23 +0200

    PHPUnit backward compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,13 @@
  */
 
 $loader = require __DIR__ . "/../vendor/autoload.php";
-$loader->addPsr4( 'YOURLS\\', __DIR__ );
+
+// PHPUnit 6 compatibility for previous versions
+if ( class_exists( 'PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
+    class_alias( 'PHPUnit\Framework\Assert',        'PHPUnit_Framework_Assert' );
+    class_alias( 'PHPUnit\Framework\TestCase',      'PHPUnit_Framework_TestCase' );
+    class_alias( 'PHPUnit\Framework\Error\Error',   'PHPUnit_Framework_Error' );
+    class_alias( 'PHPUnit\Framework\Error\Notice',  'PHPUnit_Framework_Error_Notice' );
+    class_alias( 'PHPUnit\Framework\Error\Warning', 'PHPUnit_Framework_Error_Warning' );
+}
+
