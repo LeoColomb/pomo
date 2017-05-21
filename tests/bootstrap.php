dfba4ede8e96bd09df8cc5f0a22142f9cc042223
Léo Colombaro <git@colombaro.fr>
2017-05-21 23:37:51 +0200

    Fix coding style

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,21 +10,22 @@ $loader = require __DIR__ . "/../vendor/autoload.php";
 /**
 * Returns the name of a temporary file
 */
-function temp_filename() {
+function temp_filename()
+{
     $tmp_dir = realpath(__DIR__);
     return tempnam($tmp_dir, 'pomo');
 }
 
-function replace_r_n($text) {
+function replace_r_n($text)
+{
     return str_replace("\r\n", "\n", $text);
 }
 
 // PHPUnit 6 compatibility for previous versions
-if (class_exists('PHPUnit\Runner\Version' ) && version_compare( PHPUnit\Runner\Version::id(), '6.0', '>=' ) ) {
-    class_alias('PHPUnit\Framework\Assert',        'PHPUnit_Framework_Assert');
-    class_alias('PHPUnit\Framework\TestCase',      'PHPUnit_Framework_TestCase');
-    class_alias('PHPUnit\Framework\Error\Error',   'PHPUnit_Framework_Error');
-    class_alias('PHPUnit\Framework\Error\Notice',  'PHPUnit_Framework_Error_Notice');
+if (class_exists('PHPUnit\Runner\Version') && version_compare(PHPUnit\Runner\Version::id(), '6.0', '>=')) {
+    class_alias('PHPUnit\Framework\Assert', 'PHPUnit_Framework_Assert');
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+    class_alias('PHPUnit\Framework\Error\Error', 'PHPUnit_Framework_Error');
+    class_alias('PHPUnit\Framework\Error\Notice', 'PHPUnit_Framework_Error_Notice');
     class_alias('PHPUnit\Framework\Error\Warning', 'PHPUnit_Framework_Error_Warning');
 }
-
